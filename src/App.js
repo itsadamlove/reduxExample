3b0d54d3c2d111dace10dea470244e1f7c55a612
Adam Love <adam.love127@gmail.com>
2018-08-30 10:58:45 +1000

    Added posts & postform to display and add posts fetching data from jsonplaceholderapi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Posts from './Components/Post.js'
+import Postform from './Components/Postform.js'
 
 class App extends Component {
   render() {
@@ -8,11 +10,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to this sample blog thing</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Postform />
+        <hr />
+        <Posts />
       </div>
     );
   }
